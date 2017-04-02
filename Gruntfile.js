80e6e2c5be602daa11315cd424369745e6279083
Selivanov <superexpulsion@mail.ru>
2017-04-02 10:44:52 +0300

    made small automatisation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,30 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-svgstore");
 
   grunt.initConfig({
+    clean: ["build/*"],
+
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['./fonts/**'], dest: './build'},
+          {expand: true, src: ['./*.html'], dest: './build'},
+          {expand: true, src: ['./css/**'], dest: './build'},
+          {expand: true, src: ['./img/**'], dest: './build'},
+          {expand: true, src: ['./js/**'], dest: './build'}
+        ],
+      },
+    },
+
     less: {
       style: {
         files: {
-          "css/style.css": "less/style.less"
+          "build/css/style.css": "less/style.less"
         }
       }
     },
@@ -21,10 +39,46 @@ module.exports = function(grunt) {
           processors: [
             require("autoprefixer")({browsers: [
               "last 2 versions"
-            ]})
+            ]}),
+            require("css-mqpacker")({
+              sort: true
+            }),
           ]
         },
-        src: "css/*.css"
+        src: "build/css/*.css"
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: 'gzip'
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    svgstore: {
+      options: {
+        svg: {
+          style: "display: none"
+        }
+      },
+      symbols: {
+        files: {
+          "build/img/symbols.svg": ["img/*.svg"]
+        }
+      }
+    },
+
+    svgmin: {
+      symbols: {
+        files: [{
+          expand: true,
+          src: ["build/img/*.svg"]
+        }]
       }
     },
 
@@ -47,6 +101,21 @@ module.exports = function(grunt) {
       }
     },
 
+
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true
+        },
+          files: [{
+            expand: true,
+            src: ["build/img/**/*.{png,jpg,gif}"]
+          }]
+        }
+      },
+
     watch: {
       style: {
         files: ["less/**/*.less"],
@@ -54,6 +123,8 @@ module.exports = function(grunt) {
       }
     }
   });
+
+  grunt.registerTask("symbols", ["svgmin", "svgstore"]);
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
 };
