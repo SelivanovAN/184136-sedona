ef5c0bc2fa2047444afdd7974d4c49baaa014faf
Selivanov <superexpulsion@mail.ru>
2017-04-02 14:33:54 +0300

    Поправил автоматизацию

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,41 @@
 "use strict";
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-svgmin");
+  grunt.loadNpmTasks("grunt-svgstore");
+  grunt.loadNpmTasks("grunt-csso");
 
   grunt.initConfig({
+    clean: ["build/*"],
+
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['./fonts/**'], dest: './build'},
+          {expand: true, src: ['./*.html'], dest: './build'},
+          {expand: true, src: ['./js/**'], dest: './build'}
+        ],
+      },
+      build: {
+        files: [{
+          expand: true,
+          src: ["*.html"],
+          dest: "build"
+        }]
+      },
+    },
+
     less: {
       style: {
         files: {
-          "css/style.css": "less/style.less"
+          "build/css/style.css": "less/style.less"
         }
       }
     },
@@ -19,12 +44,50 @@ module.exports = function(grunt) {
       style: {
         options: {
           processors: [
-            require("autoprefixer")({browsers: [
-              "last 2 versions"
-            ]})
+            require("autoprefixer")({
+              browsers: [
+                "last 2 versions"
+              ]
+            }),
+            require("css-mqpacker")({
+              sort: true
+            }),
           ]
         },
-        src: "css/*.css"
+        src: "build/css/*.css"
+      }
+    },
+
+    csso: {
+      dynamic_mappings: {
+        expand: true,
+        cwd: 'build/css',
+        src: ['*.css', '!*.min.css'],
+        dest: 'build/css/',
+        ext: '.min.css'
+      }
+    },
+
+    svgstore: {
+      options: {
+        svg: {
+          style: "display: none"
+        }
+      },
+      symbols: {
+        files: {
+          "build/img/symbols.svg": ["img/*.svg"]
+        }
+      }
+    },
+
+    svgmin: {
+      symbols: {
+        files: [{
+          expand: true,
+          src: "img/*.svg",
+          dest: "./build"
+        }]
       }
     },
 
@@ -32,12 +95,12 @@ module.exports = function(grunt) {
       server: {
         bsFiles: {
           src: [
-            "*.html",
-            "css/*.css"
+            "build/*.html",
+            "build/css/*.css"
           ]
         },
         options: {
-          server: ".",
+          server: "build/",
           watchTask: true,
           notify: false,
           open: true,
@@ -47,13 +110,34 @@ module.exports = function(grunt) {
       }
     },
 
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          src: "img/**/*.{png,jpg,gif}",
+          dest: "./build"
+        }]
+      }
+    },
+
     watch: {
+      html: {
+        files: ["*.html"],
+        tasks: ["copy"]
+      },
       style: {
         files: ["less/**/*.less"],
-        tasks: ["less", "postcss"]
+        tasks: ["less", "postcss", "csso"]
       }
     }
   });
 
+  grunt.registerTask("symbols", ["svgmin", "svgstore"]);
   grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask("build", ["clean", "copy", "less", "postcss", "csso", "svgmin", "imagemin"]);
 };
